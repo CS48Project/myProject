cd66ee9d2ce9368f45e7dd69f6203379e8912d2c
bhammel <bhammel@umail.ucsb.edu>
2014-01-21 23:49:29 -0800

    Improved submit form

--- food/forms.py
+++ food/forms.py
@@ -3,7 +3,40 @@ from django.forms import ModelForm
 from food.models import Food
 
 class FoodForm(ModelForm):
+	category_choices = (
+		('', 'Pick a category'),
+        ('American', 'American'),
+        ('Asian', 'Asian'),
+        ('Barbecue', 'Barbecue'),
+        ('Breakfast, Brunch & Lunch', 'Breakfast, Brunch & Lunch'),
+        ('Chinese', 'Chinese'),
+        ('Dessert', 'Dessert'),
+        ('Fast Food', 'Fast Food'),
+        ('French', 'French'),
+        ('Hamburgers & Hot Dogs', 'Hamburgers & Hot Dogs'),
+        ('Health Food', 'Health Food'),
+        ('Ice Cream & Frozen Desserts', 'Ice Cream & Frozen Desserts'),
+        ('Indian', 'Indian'),
+        ('Italian', 'Italian'),
+        ('Japanese', 'Japanese'),
+        ('Mexican', 'Mexican'),
+        ('Other', 'Other'),
+        ('Pizza', 'Pizza'),
+        ('Sandwich Shops', 'Sandwich Shops'),
+        ('Seafood', 'Seafood'),
+        ('Steak Houses', 'Steak Houses'),
+        ('Thai', 'Thai'),
+        ('Vietnamese', 'Vietnamese'),
+    )
 
-    class Meta:
-        model = Food
-        fields = ['name', 'category', 'restaurant', 'rating', 'picture']
+	name = forms.CharField(max_length=50)
+	category = forms.ChoiceField(choices=category_choices)
+	restaurant = forms.CharField(max_length=50)
+	price = forms.DecimalField(min_value=0, max_digits=5, decimal_places=2)
+	rating = forms.DecimalField(max_value=5, min_value=0, max_digits=2,
+								decimal_places=1, required=False)
+	picture = forms.FileField(required=False)
+
+	class Meta:
+		model = Food
+		fields = ['name', 'category', 'restaurant', 'price', 'rating', 'picture']
--- food/models.py
+++ food/models.py
@@ -5,46 +5,12 @@ def get_upload_file_name(instance, filename):
     return "uploaded_files/%s" % (filename)
 
 # Create your models here.
-class MinMaxFloat(models.FloatField):
-    def __init__(self, min_value=None, max_value=None, *args, **kwargs):
-        self.min_value, self.max_value = min_value, max_value
-        super(MinMaxFloat, self).__init__(*args, **kwargs)
-
-    def formfield(self, **kwargs):
-        defaults = {'min_value': self.min_value, 'max_value' : self.max_value}
-        defaults.update(kwargs)
-        return super(MinMaxFloat, self).formfield(**defaults)
-
 class Food(models.Model):
-    category_choices = (
-        ('American', 'American'),
-        ('Asian', 'Asian'),
-        ('Barbecue', 'Barbecue'),
-        ('Breakfast, Brunch & Lunch', 'Breakfast, Brunch & Lunch'),
-        ('Chinese', 'Chinese'),
-        ('Dessert', 'Dessert'),
-        ('Fast Food', 'Fast Food'),
-        ('French', 'French'),
-        ('Hamburgers & Hot Dogs', 'Hamburgers & Hot Dogs'),
-        ('Health Food', 'Health Food'),
-        ('Ice Cream & Frozen Desserts', 'Ice Cream & Frozen Desserts'),
-        ('Indian', 'Indian'),
-        ('Italian', 'Italian'),
-        ('Japanese', 'Japanese'),
-        ('Mexican', 'Mexican'),
-        ('Other', 'Other'),
-        ('Pizza', 'Pizza'),
-        ('Sandwich Shops', 'Sandwich Shops'),
-        ('Seafood', 'Seafood'),
-        ('Steak Houses', 'Steak Houses'),
-        ('Thai', 'Thai'),
-        ('Vietnamese', 'Vietnamese'),
-    )
-
     name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, choices=category_choices, default='Other')
+    category = models.CharField(max_length=50)
     restaurant = models.CharField(max_length=50)
-    rating = MinMaxFloat(max_value=5.0, min_value=0.0, default=3.0)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
     likes = models.IntegerField(default=0)
     picture = models.FileField(upload_to=get_upload_file_name)
     
