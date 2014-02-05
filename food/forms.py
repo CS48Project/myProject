7dc224cd82444ed7215e810c9c68a0b444d13233
Andrew Pang <andrewpang94@gmail.com>
2014-02-05 12:15:46 -0800

    Update food template

--- food/forms.py
+++ food/forms.py
@@ -4,10 +4,10 @@ from food.models import Food, Category
 
 class FoodForm(ModelForm):
 	name = forms.CharField(max_length=50)
+	restaurant = forms.CharField(max_length=50)
 	category = forms.ModelChoiceField(queryset=Category.objects.order_by('name'),
                                       empty_label="Pick a category")
-	restaurant = forms.CharField(max_length=50)
-	price = forms.DecimalField(min_value=0, max_digits=5, decimal_places=2)
+	price = forms.DecimalField(min_value=0, max_digits=5, decimal_places=2, required=False)
 	picture = forms.FileField(required=False)
 
 	class Meta:
--- food/models.py
+++ food/models.py
@@ -12,7 +12,7 @@ class Food(models.Model):
     slug = models.SlugField()
     category = models.ForeignKey('Category')
     restaurant = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     picture = models.FileField(upload_to=get_upload_file_name, blank=True, null=True)
 
     def save(self, *args, **kwargs):
