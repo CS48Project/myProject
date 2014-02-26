3cbe90591b0cebdaa83ba4404d3a6eefe72ae61e
bhammel <bhammel@umail.ucsb.edu>
2014-02-25 20:05:11 -0800

    Added some categories, restaurants, and food to database for testing layout

--- food/models.py
+++ food/models.py
@@ -8,13 +8,6 @@ from random import choice
 from ratings.handlers import ratings
 from ratings.forms import StarVoteForm
 
-def get_upload_file_name(instance, filename):
-    """
-    Helper function that creates the uploaded file in the uploaded_files directory
-    in order to be served as a static file on the site.
-    """
-    return "uploaded_files/%s" % (filename)
-
 # Create your models here.
 class Food(models.Model):
     """
@@ -25,7 +18,7 @@ class Food(models.Model):
     category = models.ForeignKey('Category')
     restaurant = models.ForeignKey('Restaurant')
     price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    picture = models.FileField(upload_to=get_upload_file_name, blank=True, null=True)
+    picture = models.FileField(upload_to="uploaded_files/food_pics", blank=True, null=True)
 
     # Automatically slugifies the Food object's name upon creation.
     def save(self, *args, **kwargs):
@@ -48,6 +41,7 @@ class Category(models.Model):
     """
     name = models.CharField(max_length=50)
     slug = models.SlugField()
+    picture = models.FileField(upload_to="uploaded_files/category_pics", blank=True, null=True)
 
     # Returns the Category object's URL.
     def get_absolute_url(self):
@@ -65,6 +59,7 @@ class Restaurant(models.Model):
     """
     name = models.CharField(max_length=50)
     slug = models.SlugField()
+    picture = models.FileField(upload_to="uploaded_files/restaurant_pics", blank=True, null=True)
 
     # Returns the Restaurant object's URL.
     def get_absolute_url(self):
