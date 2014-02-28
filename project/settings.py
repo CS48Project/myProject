50fde8872deecff7e76b2c5d581ccc7fc07fd07a
bhammel <bhammel@umail.ucsb.edu>
2014-02-27 21:02:05 -0800

    reverted last changes

--- project/settings.py
+++ project/settings.py
@@ -120,18 +120,3 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [('assets', os.path.join(BASE_DIR, 'static'))]
-
-
-from os import environ
-from urlparse import urlparse
-
-if environ.has_key('DATABASE_URL'):
-    url = urlparse(environ['DATABASE_URL'])
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': url.path[1:],
-        'USER': url.username,
-        'PASSWORD': url.password,
-        'HOST': url.hostname,
-        'PORT': url.port,
-    }
