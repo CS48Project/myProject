0ec716c0f246581ce90acf12bc26f2f0eec31f49
bhammel <bhammel@umail.ucsb.edu>
2014-02-27 20:54:58 -0800

    postresql settings added

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates'),
                  os.path.join(BASE_DIR, 'food/templates'),
@@ -120,3 +120,18 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [('assets', os.path.join(BASE_DIR, 'static'))]
+
+
+from os import environ
+from urlparse import urlparse
+
+if environ.has_key('DATABASE_URL'):
+    url = urlparse(environ['DATABASE_URL'])
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
