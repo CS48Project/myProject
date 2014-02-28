4debef1559ef4a6683027e85ba32c829d9aff723
bhammel <bhammel@umail.ucsb.edu>
2014-02-27 21:06:59 -0800

    still trying to get postgresql to work

--- project/settings.py
+++ project/settings.py
@@ -120,3 +120,21 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [('assets', os.path.join(BASE_DIR, 'static'))]
+
+
+import psycopg2
+import urlparse
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
