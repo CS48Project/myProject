a1e357f485e86d6a22c205f9a54abe7acf662229
bhammel <bhammel@umail.ucsb.edu>
2014-02-28 15:18:21 -0800

    changed static files settings

--- project/settings.py
+++ project/settings.py
@@ -115,11 +115,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [('assets', os.path.join(BASE_DIR, 'static'))]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
