6f65fe0cdd06b8c252b9bc411ab646cf81dfe8d3
bhammel <bhammel@umail.ucsb.edu>
2014-02-28 10:10:02 -0800

    Turned off debug

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'qgd-0(ckc^q+0w2(k%jxt8ywtapv-r-@6x-de2sb#$afzk*sqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
