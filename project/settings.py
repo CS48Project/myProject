cc8b9ca15172fba11d2b532d0e1a30b7ed16953f
bhammel <bhammel@umail.ucsb.edu>
2014-03-06 15:50:25 -0800

    added postgresql to settings

--- project/settings.py
+++ project/settings.py
@@ -166,3 +166,17 @@ LOGGING = {
         },
     }
 }
+
+from os import environ
+from urlparse import urlparse
+
+if environ.has_key('DATABASE_URL'):
+    url = urlparse(environ['DATABASE_URL'])
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+    }
