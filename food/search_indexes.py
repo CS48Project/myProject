45ea79edcbbcf378b987c4e9cf1c3cf4c70f10ba
bhammel <bhammel@umail.ucsb.edu>
2014-03-13 13:21:14 -0700

    fixed indentation error

--- food/search_indexes.py
+++ food/search_indexes.py
@@ -4,11 +4,11 @@ from food.models import Food
 class FoodIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document=True, use_template=True)
 
-    content_auto = indexes.EdgeNgramField(model_attr='name')
+	content_auto = indexes.EdgeNgramField(model_attr='name')
 
     def get_model(self):
     	return Food
 
-    def index_queryset(self, using=None):
-    	"""Used when the entire index for the model is updated."""
-    	return self.get_model().objects.all()
+	def index_queryset(self, using=None):
+		"""Used when the entire index for the model is updated."""
+		return self.get_model().objects.all()
